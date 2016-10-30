114b5af85fb27d7dfca83db540a8747d53450ee2
David Butz <davidthomasbutz@gmail.com>
2016-10-30 17:51:32 -0400

    additional commits to project

---
+++ iOS/Boat Aware/createLeagueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  createLeagueViewController.swift
+//  Christmas Fam Duels
+//
+//  Created by Dave Butz on 10/28/16.
+//  Copyright © 2016 Thrive Engineering. All rights reserved.
+//
+
+import UIKit
+
+class createLeagueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
